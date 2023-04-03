839ccca8840f48bc9caff10b2a3dcbaed1bcb8f2
minhnguyen0910 <minhn091099@gmail.com>
2023-04-03 16:35:28 +0700

    update

---
+++ ss_06/blog/src/main/java/com/codegym/blog/controller/BLogController.java
@@ -0,0 +1,51 @@
+package com.codegym.blog.controller;
+
+import com.codegym.blog.model.BLog;
+import com.codegym.blog.service.IBLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/blog")
+public class BLogController {
+    @Autowired
+    private IBLogService ibLogService;
+
+    @GetMapping("")
+    public String showList(Model model) {
+        model.addAttribute("listBLog", ibLogService.findAll());
+        return "list";
+    }
+
+    @GetMapping("create")
+    public String showFormCreate(Model model) {
+        model.addAttribute("list", new BLog());
+        return "create";
+    }
+
+    @PostMapping("create")
+    public String create(BLog bLog) {
+        ibLogService.save(bLog);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("delete")
+    public String delete(@RequestParam(name = "id") Integer id) {
+        ibLogService.delete(id);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("edit/{id}")
+    public String showFormEdit(@PathVariable(name = "id") Integer id, Model model) {
+        model.addAttribute("blog", ibLogService.findById(id));
+        return "edit";
+    }
+
+    @PostMapping("edit")
+    public String edit(BLog bLog) {
+        ibLogService.edit(bLog);
+        return "redirect:/blog";
+    }
+}
