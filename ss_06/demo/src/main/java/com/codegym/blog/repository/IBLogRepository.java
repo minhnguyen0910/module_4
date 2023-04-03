bb6f038e0736f783c7f53716b06b012faf4e2928
minhnguyen0910 <minhn091099@gmail.com>
2023-04-03 16:43:47 +0700

    update

--- ss_06/demo/src/main/java/com/codegym/blog/repository/IBLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.blog.repository;
-
-import com.codegym.blog.model.BLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-
-public interface IBLogRepository extends PagingAndSortingRepository<BLog, Integer> {
-}
